239c25dd8a0fba962984cfe3a16384c337e4ad7c
Anton Razmakhnin <anton.razmakhnin@devfactory.com>
2021-06-08 21:42:13 +0400

    Can drow something now

--- RReader/RReader/MainPage.xaml.cs
+++ RReader/RReader/MainPage.xaml.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace RReader
@@ -16,10 +22,44 @@ namespace RReader
         }
 
         int count = 0;
-        private void Handle_Clicked(object sender, System.EventArgs e)
+        private async void Handle_Clicked(object sender, System.EventArgs e)
         {
-            count++;
-            ((Button)sender).Text = $"You clicked {count} times.";
+            var status = await Permissions.CheckStatusAsync<Permissions.StorageRead>();
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<Permissions.StorageRead>();
+                if (status != PermissionStatus.Granted)
+                {
+                    ((Button)sender).Text = "Nothing because " + status;
+                    return;
+                }
+            }
+
+            var text = File.ReadAllText("/sdcard/Download/1.fb2");
+            var doc = XDocument.Parse(text);
+            ((Button)sender).Text = doc.Root.Name.LocalName;
+        }
+
+        private void SKCanvasView_OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var info = e.Info;
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            var paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Red.ToSKColor(),
+                StrokeWidth = 25
+            };
+
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+
+            paint.Style = SKPaintStyle.Fill;
+            paint.Color = SKColors.Blue;
+            canvas.DrawCircle(e.Info.Width / 2, e.Info.Height / 2, 100, paint);
         }
     }
 }
